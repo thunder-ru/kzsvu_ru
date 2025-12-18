11c6f97474f66af93fbaec76b3918f727cfd83e7
thunder-ru <rosanov.danila2016@yandex.ru>
2025-12-18 21:38:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,24 @@ const hamburger = document.getElementById('hamburger');
 const hamburgerMenu = document.getElementById('hamburger-menu');
 
 hamburger.addEventListener('mouseenter', () => {
-    hamburgerMenu.classList.add('visible');
+    hamburgerMenu.style.opacity = '1';
+    hamburgerMenu.style.visibility = 'visible';
 });
 
 hamburgerMenu.addEventListener('mouseenter', () => {
-    hamburgerMenu.classList.add('visible');
+    hamburgerMenu.style.opacity = '1';
+    hamburgerMenu.style.visibility = 'visible';
 });
+
+const hideHamburgerMenu = () => {
+    hamburgerMenu.style.opacity = '0';
+    hamburgerMenu.style.visibility = 'hidden';
+};
 
 hamburger.addEventListener('mouseleave', () => {
     setTimeout(() => {
-        if (!hamburgerMenu.matches(':hover')) {
-            hamburgerMenu.classList.remove('visible');
-        }
+        if (!hamburgerMenu.matches(':hover')) hideHamburgerMenu();
     }, 100);
 });
 
-hamburgerMenu.addEventListener('mouseleave', () => {
-    hamburgerMenu.classList.remove('visible');
-});
+hamburgerMenu.addEventListener('mouseleave', hideHamburgerMenu);
