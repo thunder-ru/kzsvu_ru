e32216403edb358511febd28b814530366e90602
thunder-ru <rosanov.danila2016@yandex.ru>
2025-12-18 21:34:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
 const hamburger = document.getElementById('hamburger');
 const hamburgerMenu = document.getElementById('hamburger-menu');
 
-let timeout;
-
 hamburger.addEventListener('mouseenter', () => {
-    clearTimeout(timeout);
-    hamburgerMenu.style.opacity = '1';
-    hamburgerMenu.style.visibility = 'visible';
-    hamburgerMenu.style.transform = 'translateY(0)';
+    hamburgerMenu.classList.add('visible');
 });
 
 hamburgerMenu.addEventListener('mouseenter', () => {
-    clearTimeout(timeout);
-    hamburgerMenu.style.opacity = '1';
-    hamburgerMenu.style.visibility = 'visible';
-    hamburgerMenu.style.transform = 'translateY(0)';
+    hamburgerMenu.classList.add('visible');
 });
 
 hamburger.addEventListener('mouseleave', () => {
-    timeout = setTimeout(() => {
+    setTimeout(() => {
         if (!hamburgerMenu.matches(':hover')) {
-            hamburgerMenu.style.opacity = '0';
-            hamburgerMenu.style.visibility = 'hidden';
-            hamburgerMenu.style.transform = 'translateY(-10px)';
+            hamburgerMenu.classList.remove('visible');
         }
-    }, 200);
+    }, 100);
 });
 
 hamburgerMenu.addEventListener('mouseleave', () => {
-    hamburgerMenu.style.opacity = '0';
-    hamburgerMenu.style.visibility = 'hidden';
-    hamburgerMenu.style.transform = 'translateY(-10px)';
+    hamburgerMenu.classList.remove('visible');
 });
